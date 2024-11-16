034fe12c7a92ec390c37dfd4d4fb9a2c31331ab3
Saleh Jaber <salehj@gatech.edu>
2024-11-16 00:44:55 +0000

    lab completed

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -14,7 +14,19 @@ public class Fibonacci {
      * @param n an iteration of the fibonacci sequence.
      * @return the nth number of fibonacci sequence.
      */
-    public int fib(int n){
-        return 0;
+    public int fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+    
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 }
